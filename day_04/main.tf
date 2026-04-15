a29ad715ff2cd345231e11c21a3f9898a06d6431
kaidranzerr <ashstark68@gmail.com>
2026-04-15 16:18:20 +0530

    working understood!!

---
+++ day_04/main.tf
@@ -0,0 +1,29 @@
+# terraform Terraform needs a memory of what it created
+# That memory = .tfstate file  
+
+# Your .tf file = what you WANT
+# .tfstate = what Terraform KNOWS already exists
+
+# What State File Stores
+# Created resources (like **Amazon S3 bucket)
+# Metadata
+# Resource IDs
+# ⚠️ Sensitive data:
+# Account ID
+# Secrets
+# Config details  
+
+# Remote Backend
+
+# 👉 Store state remotely (best practice) 
+
+# 👉 Terraform does NOT modify the state file directly first
+# 👉 It modifies AWS infra first, then updates the state file
+
+# The state file is:
+
+# Downloaded locally
+# Updated locally
+# Then uploaded back to S3
+
+# NOT directly edited in S3.
