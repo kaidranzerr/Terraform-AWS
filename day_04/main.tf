9b064d17cbb315124d386f7c1c6ee8779d96e128
kaidranzerr <ashstark68@gmail.com>
2026-04-15 16:24:57 +0530

    state locking understood!

--- day_04/main.tf
+++ day_04/main.tf
@@ -27,7 +27,31 @@
 # Then uploaded back to S3
 
 # NOT directly edited in S3.
-
 # You DO NOT update the state file manually
 
 # 👉 Terraform updates it automatically after changing AWS
+# state locking --> only one person can edit at a time 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# configure AWS provider 
+provider "aws" {
+  region = "us-east-1"
+}
+
+# create S3 bucket 
+resource "aws_s3_bucket" "first_bucket" {
+    bucket = "techtuts1"
+
+    tags = {
+        Name = "My bucket"
+        Environment = "Dev"
+    }
+}
