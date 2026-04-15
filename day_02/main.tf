00762027a78060f575f41f8b05d48d0bd9e2e664
kaidranzerr <ashstark68@gmail.com>
2026-04-15 13:38:03 +0530

    vpc created!

--- day_02/main.tf
+++ day_02/main.tf
@@ -12,3 +12,23 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# create a VPC --> creating a resource
+# aws_vpc is the resource type 
+# example is the local name of the resource through which we will refer it internally within terraform
+resource "aws_vpc" "example"{
+    cidr_block = "10.0.0.0/16"
+}
+
+# multiple provider versions
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+}
